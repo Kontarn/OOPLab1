a7e41478492f9daaba1ae7945ef5eacc1766498b
Kontarn <zek123457@gmail.com>
2023-03-03 22:28:54 +0300

    Добавил функцию считывания данных из txt  файла и записи в другой файл

--- StankinOOPLabs/StankinOOPLabs.cpp
+++ StankinOOPLabs/StankinOOPLabs.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+void DataBaseFill() {
+	string str; // Переменная для временного хранения данных InputText.txt
+	ifstream fin;
+	ofstream fout;
+	fin.exceptions(ifstream::badbit | ifstream::failbit);
+	try
+	{
+		cout << "Попытк открыть файл 'InputText.txt': " << endl;
+		fin.open("InputText.txt");
+		cout << "Успешно" << endl;
+	}
+	catch (const ifstream::failure& ex)
+	{
+		cout << ex.what() << endl;
+		cout << ex.code() << endl;
+		cout << "Ошибка открытия файла" << endl;
+	}
+	try
+	{
+		cout << "Попытк открыть файл 'MyLittleBD.txt': " << endl;
+		fout.open("MyLittleBD.txt");
+		cout << "Успешно" << endl;
+	}
+	catch (const ofstream::failure& ex)
+	{
+		cout << ex.what() << endl;
+		cout << ex.code() << endl;
+		cout << "Ошибка открытия файла" << endl;
+	}
+
+	while (!fin.eof()) {
+		str = "";
+		getline(fin, str);
+		fout << str << "\n";
+	}
+	fout.close();
+	fin.close();
+}
 int main()
 {
-    string str;
-    cin >> str;
-    std::cout << str;
+	setlocale(LC_ALL, "ru");
+	DataBaseFill();
+	return 0;
 }
