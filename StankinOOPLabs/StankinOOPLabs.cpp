069ab5f9b49ad5aa0cd0582447f87039388acf05
Kontarn <zek123457@gmail.com>
2023-03-07 13:28:45 +0300

    Добавил то же самое для 2-4 групп

--- StankinOOPLabs/StankinOOPLabs.cpp
+++ StankinOOPLabs/StankinOOPLabs.cpp
@@ -41,8 +41,6 @@ public:
 			cout << ex.code() << endl;
 			cout << "Ошибка открытия файла" << endl;
 		}
-		//fin.open("InputText.txt");
-		//fout.open("Group1.txt");
 	}
 	void DataBaseFill() override {
 		string len;
@@ -62,11 +60,167 @@ public:
 		fout.close();
 	}
 };
+class Group2 :public Groups {
+	string str;
+	ifstream fin;
+	ofstream fout;
+public:
+	Group2(string nameInpFile, string GroupFile) {
+		fin.exceptions(ifstream::badbit | ifstream::failbit);
+		try
+		{
+			cout << "Попытк открыть файл 'InputText.txt': " << endl;
+			fin.open(nameInpFile);
+			cout << "Успешно" << endl;
+		}
+		catch (const ifstream::failure& ex)
+		{
+			cout << ex.what() << endl;
+			cout << ex.code() << endl;
+			cout << "Ошибка открытия файла" << endl;
+		}
+		try
+		{
+			cout << "Попытк открыть файл 'Group2.txt': " << endl;
+			fout.open(GroupFile);
+			cout << "Успешно" << endl;
+		}
+		catch (const ofstream::failure& ex)
+		{
+			cout << ex.what() << endl;
+			cout << ex.code() << endl;
+			cout << "Ошибка открытия файла" << endl;
+		}
+	}
+	void DataBaseFill() override {
+		string len;
+		while (!fin.eof()) {
+			str = "";
+			getline(fin, str);
+			//len = str.size() - 1;
+			len = str.substr(str.size() - 1);
+			if (len == "2") {
+				fout << str << "\n";
+			}
+
+		}
+	}
+	~Group2() {
+		fin.close();
+		fout.close();
+	}
+};
+class Group3 :public Groups {
+	string str;
+	ifstream fin;
+	ofstream fout;
+public:
+	Group3(string nameInpFile, string GroupFile) {
+		fin.exceptions(ifstream::badbit | ifstream::failbit);
+		try
+		{
+			cout << "Попытк открыть файл 'InputText.txt': " << endl;
+			fin.open(nameInpFile);
+			cout << "Успешно" << endl;
+		}
+		catch (const ifstream::failure& ex)
+		{
+			cout << ex.what() << endl;
+			cout << ex.code() << endl;
+			cout << "Ошибка открытия файла" << endl;
+		}
+		try
+		{
+			cout << "Попытк открыть файл 'Group3.txt': " << endl;
+			fout.open(GroupFile);
+			cout << "Успешно" << endl;
+		}
+		catch (const ofstream::failure& ex)
+		{
+			cout << ex.what() << endl;
+			cout << ex.code() << endl;
+			cout << "Ошибка открытия файла" << endl;
+		}
+	}
+	void DataBaseFill() override {
+		string len;
+		while (!fin.eof()) {
+			str = "";
+			getline(fin, str);
+			//len = str.size() - 1;
+			len = str.substr(str.size() - 1);
+			if (len == "3") {
+				fout << str << "\n";
+			}
+
+		}
+	}
+	~Group3() {
+		fin.close();
+		fout.close();
+	}
+};
+class Group4 :public Groups {
+	string str;
+	ifstream fin;
+	ofstream fout;
+public:
+	Group4(string nameInpFile, string GroupFile) {
+		fin.exceptions(ifstream::badbit | ifstream::failbit);
+		try
+		{
+			cout << "Попытк открыть файл 'InputText.txt': " << endl;
+			fin.open(nameInpFile);
+			cout << "Успешно" << endl;
+		}
+		catch (const ifstream::failure& ex)
+		{
+			cout << ex.what() << endl;
+			cout << ex.code() << endl;
+			cout << "Ошибка открытия файла" << endl;
+		}
+		try
+		{
+			cout << "Попытк открыть файл 'Group4.txt': " << endl;
+			fout.open(GroupFile);
+			cout << "Успешно" << endl;
+		}
+		catch (const ofstream::failure& ex)
+		{
+			cout << ex.what() << endl;
+			cout << ex.code() << endl;
+			cout << "Ошибка открытия файла" << endl;
+		}
+	}
+	void DataBaseFill() override {
+		string len;
+		while (!fin.eof()) {
+			str = "";
+			getline(fin, str);
+			//len = str.size() - 1;
+			len = str.substr(str.size() - 1);
+			if (len == "4") {
+				fout << str << "\n";
+			}
+
+		}
+	}
+	~Group4() {
+		fin.close();
+		fout.close();
+	}
+};
 int main()
 {
 	setlocale(LC_ALL, "ru");
 	Group1 gr1("InputText.txt", "Group1.txt");
 	gr1.DataBaseFill();
+	Group2 gr2("InputText.txt", "Group2.txt");
+	gr2.DataBaseFill();
+	Group3 gr3("InputText.txt", "Group3.txt");
+	gr3.DataBaseFill();
+	Group4 gr4("InputText.txt", "Group4.txt");
+	gr4.DataBaseFill();
 	return 0;
 	_CrtDumpMemoryLeaks();
 }
