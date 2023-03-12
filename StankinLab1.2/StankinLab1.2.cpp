2462db2f9e8c7ecbdd17842fc99ed67b959a2a15
Kontarn <zek123457@gmail.com>
2023-03-12 16:29:08 +0300

    Сделал копирование данных из вектора BD1 в BD2, а также удалил BD1

--- StankinLab1.2/StankinLab1.2.cpp
+++ StankinLab1.2/StankinLab1.2.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 class Parent {
-    std::string *str;
 public:
+    string* str;
     Parent(){
-        str = new string;
+        this->str = new string();
     }
-    ~Parent() {
+    Parent(const Parent &other) {
+        this->str = other.str;
+    }
+    virtual ~Parent() {
         delete str;
+    }
+    virtual Parent* Copy() {
+        return new Parent(*this);
     }
 };
 class Child : public Parent{
-    Child();
-    ~Child(){}
+public:
+    Child() {}
+    ~Child() override{}  
 };
-
 int main()
 {
-    vector <Parent> BD1;
+    setlocale(LC_ALL, "ru");
+    vector<Parent*>* BD1 = new vector<Parent*>;
+    vector<Parent*>* BD2 = new vector<Parent*>;
+    cout << "Вывод вектора BD1: " << endl;
+    for (int i = 0; i < 4; i++) {
+        BD1->push_back((i % 2 == 0) ? new Parent() : new Child());
+    }
+    for (vector <Parent*> :: iterator it = BD1->begin(); it != BD1->end(); ++it) {
+        cout << *it << endl;
+    }
+    cout << "Вывод вектора BD2: " << endl;
+    for (int i = 0; i < BD1->size(); i++) {
+        Parent *p = (*BD1)[i]->Copy();
+        BD2->push_back(p);
+    }
+    for (auto it = BD2->begin(); it != BD2->end(); ++it) {
+        cout << *it << endl;
+    }
+    delete BD2;
+    BD1->~vector();
+    return 0;
+    _CrtDumpMemoryLeaks();
 }
 
