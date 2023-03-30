56f36ad9203493044bb950f7593ddcbc38cf0d35
Kontarn <zek123457@gmail.com>
2023-03-30 09:24:48 +0300

    Делаю заполнеине БД с помощью свойств классов библиотек 112 и 362

--- ObjOProgLab2.1/ObjOProgLab2.1.cpp
+++ ObjOProgLab2.1/ObjOProgLab2.1.cpp
@@ -8,137 +8,133 @@
 #include <map>
 using namespace std;
 
-
-class Literature {
-public:
-    Literature(){}
-    ~Literature(){}
-};
-//class Book : public Literature {
-//    
-//public:
-//    std::string* nameBook; // Название книги
-//    int* yearOfRelease; // Дата выхода книги
-//    int* numberOfPages; // Количество страниц
-//
-//    Book(/*string namebook, int yearOfRelease, int numberOfPages*/) :         
-//        nameBook(new std::string),
-//        yearOfRelease(new int),        
-//        numberOfPages(new int) 
-//    {
-//        /*this->nameBook = namebook;
-//        this->yearOfRelease = yearOfRelease;
-//        this->numberOfPages = numberOfPages;*/
-//
-//    }
-//
-//    /*void fillDBLib362() override {
-//
-//    }*/
-//    virtual ~Book(){
-//        delete yearOfRelease, nameBook, numberOfPages;
-//    }
-//};
-//class Brochures : public Literature{
-//public:
-//    int* yearOfRelease; // Дата выхода книги
-//    std::string* nameBook; // Название книги
-//    int* numberOfPages; // Количество страниц
-//    Brochures() : yearOfRelease(new int),
-//        nameBook(new std::string),
-//        numberOfPages(new int) {
-//    }
-//    virtual ~Brochures() {
-//        delete yearOfRelease, nameBook, numberOfPages;
-//    }
-//};
-//class Newspapers : public Literature {
-//public:
-//    int* yearOfRelease; // Дата выхода книги
-//    std::string* nameBook; // Название книги
-//    int* numberOfPages; // Количество страниц
-//    Newspapers() : yearOfRelease(new int),
-//        nameBook(new std::string),
-//        numberOfPages(new int) {
-//    }
-//    virtual ~Newspapers() {
-//        delete yearOfRelease, nameBook, numberOfPages;
-//    }
-//};
-//class Magazines : public Literature {
-//public:
-//    int* yearOfRelease; // Дата выхода книги
-//    std::string* nameBook; // Название книги
-//    int* numberOfPages; // Количество страниц
-//    Magazines() : yearOfRelease(new int),
-//        nameBook(new std::string),
-//        numberOfPages(new int) {
-//    }
-//    virtual ~Magazines() {
-//        delete yearOfRelease, nameBook, numberOfPages;
-//    }
-//};
 class Lib362{
-    std::string nameBook; // Название книги
-    int yearOfRelease; // Дата выхода книги
-    int numberOfPages; // Количество страниц
-    string Library362 = "Lib362.txt";
+    std::string nameBook;   // Название книги
+    int yearOfRelease;      // Дата выхода книги
+    int numberOfPages;      // Количество страниц
+    string typeBook;        // Тип книги
+    
     string InpText; // Переменная для временного хранения строк из файла
     ifstream fin;
     ofstream fout;
+    string InputFileBD;
 public:
-    Lib362(string InputFileBD) {
-        fout.exceptions(ofstream::badbit | ofstream::failbit);
+    string Library362 = "Lib362.txt";
+    Lib362(/*string InputFileBD*/) {
+        fout.open(Library362, fstream::out | fstream::ate/*, fstream::out | fstream::app*/);
+        /*fout.exceptions(ofstream::badbit | ofstream::failbit);
         try {
-            fout.open("Lib362.txt");
+            fout.open(Library362);
         }
         catch (const ofstream::failure& ex) {
             cout << ex.what() << endl;
             cout << ex.code() << endl;
             cout << "Ошибка открытия файла Lib362.txt" << endl;
-        }
-        fin.exceptions(ifstream::badbit | ifstream::failbit);
-        try {
-            fin.open(InputFileBD);
-        }
-        catch (const ifstream::failure& ex) {
-            cout << ex.what() << endl;
-            cout << ex.code() << endl;
-            cout << "Ошибка открытия файла BD.txt" << endl;
-        }
+        }*/
+    }
+    string GetLib362() {
+        return Library362;
+    }
+    void delBeforeStr() {
+
+    }
+    void SetInpText(string InpText) {
+        this->InpText = InpText;
+        string TInpText;
+        int pos = 0;
+        // Получаем тип книги
+        pos = InpText.find(";", pos);
+        TInpText = InpText;
+        TInpText.erase(pos);
+        typeBook = TInpText;
+        // Получаем название книги
+        TInpText = this->InpText.erase(0, pos + 2);
+        pos = TInpText.find(",", pos);
+        TInpText = this->InpText.erase(pos);
+        nameBook = TInpText;
+        // Получаем год выпуска
+        pos = 0;
+        pos = InpText.find(",", pos);
+        TInpText = InpText;
+        TInpText = TInpText.erase(0, pos + 2);
+        pos = TInpText.rfind(",", pos);
+        TInpText = TInpText.erase(pos);
+        yearOfRelease = stoi(TInpText);
+        // Получаем количество страниц
+        pos = 0;
+        pos = InpText.rfind(",");
+        TInpText = InpText;
+        TInpText = TInpText.erase(0, pos + 2);
+        numberOfPages = stoi(TInpText);
+        
+        fout << typeBook << " " << nameBook << " " << yearOfRelease << " " << numberOfPages << endl;
+        
     }
     ~Lib362() {
-        fin.close();
+        fout.close();
     }
 };
 class Lib112 {
-    std::string nameBook; // Название книги
-    int yearOfRelease; // Дата выхода книги
-    int numberOfPages; // Количество страниц
-    fstream fs;
+    std::string nameBook;   // Название книги
+    int yearOfRelease;      // Дата выхода книги
+    int numberOfPages;      // Количество страниц
+    string typeBook;        // Тип книги
+    
+    string InpText;
+    ofstream fout;
+
 public:
-    Lib112(string InputFileBD) {
-        fs.exceptions(fstream::badbit | fstream::failbit);
-        try {
-            fs.open("Lib112.txt", fstream::in | fstream::out);
-        }
-        catch (const fstream::failure& ex) {
-            cout << ex.code() << endl;
-            cout << ex.what() << endl;
-            cout << "Ошибка открытия файла Lib112.txt" << endl;
-        }
+    string Library112 = "Lib112.txt";
+    Lib112() {
+        fout.open(Library112, fstream::out | fstream::ate/*, fstream::out | fstream::app*/);
+    }
+    string GetLib112() {
+        return Library112;
+    }
+    void SetInpText(string InpText) {
+        this->InpText = InpText;
+        string TInpText;
+        int pos = 0;
+        // Получаем тип книги
+        pos = InpText.find(";", pos);
+        TInpText = InpText;
+        TInpText.erase(pos);
+        typeBook = TInpText;
+        // Получаем название книги
+        TInpText = this->InpText.erase(0, pos + 2);
+        pos = TInpText.find(",", pos);
+        TInpText = this->InpText.erase(pos);
+        nameBook = TInpText;
+        // Получаем год выпуска
+        pos = 0;
+        pos = InpText.find(",", pos);
+        TInpText = InpText;
+        TInpText = TInpText.erase(0, pos + 2);
+        pos = TInpText.rfind(",", pos);
+        TInpText = TInpText.erase(pos);
+        yearOfRelease = stoi(TInpText);
+        // Получаем количество страниц
+        pos = 0;
+        pos = InpText.rfind(",");
+        TInpText = InpText;
+        TInpText = TInpText.erase(0, pos + 2);
+        numberOfPages = stoi(TInpText);
+        
+        fout << typeBook << " " << nameBook << " " << yearOfRelease << " " << numberOfPages << endl;
+        
+    }
+
+    ~Lib112() {
+        fout.close();
     }
 };
-void fillDBLibs(string InputFileBD, string &OutLib362, string &OutLib112) {
-    
+void fillDBLibs(string InputFileBD, string OutLib362, string OutLib112) {
+    Lib362 lb362;
+    Lib112 lb112;
     int randNum;
     string InpText;
-    ofstream fout112;
-    ofstream fout362;
     ifstream fin;
     fin.open(InputFileBD);
-    fout112.open(OutLib362);
-    fout362.open(OutLib112);
     while (!fin.eof()) {
         srand(time(0));
         randNum = rand() % 3;
@@ -146,101 +142,29 @@ void fillDBLibs(string InputFileBD, string &OutLib362, string &OutLib112) {
         case 0:
             InpText = "";
             getline(fin, InpText);
-            fout112 << InpText << endl;
+            //lb362.SetInpText(InpText);
+            //lb112.SetParametrs(InpText);
+            lb112.SetInpText(InpText);
+            //fout112 << InpText << endl;
             break;
         case 1:
             InpText = "";
             getline(fin, InpText);
-            fout362 << InpText << endl;
+            lb362.SetInpText(InpText);
+            //fout362 << InpText << endl;
             break;
         case 2:
             InpText = "";
             getline(fin, InpText);
-            fout112 << InpText << endl;
-            fout362 << InpText << endl;
+            lb112.SetInpText(InpText);
+            lb362.SetInpText(InpText);
             break;
         default:
             break;
         }
     }
     fin.close();
-    fout112.close();
-    fout362.close();
-    //int randCBook;
-    //for (int i = 0; i < 10; i++) {
-    //    
-    //    randNum = rand() % 3;
-    //    randCBook = rand() % 4;
-    //    switch (randNum)
-    //    {
-    //    case 0:
-    //        switch (randCBook)
-    //        {
-    //        case 0:
-    //            /*Book bk("Book1", 1911, 2);*/
-    //            db362.lb362.push_back(new Book);
-    //            
-    //            break;
-    //        case 1:
-    //            db362.lb362.push_back(new Brochures);
-    //            break;
-    //        case 2:
-    //            db362.lb362.push_back(new Newspapers);
-    //            break;
-    //        case 3:
-    //            db362.lb362.push_back(new Magazines);
-    //            break;
-    //        default:
-    //            break;
-    //        }
-    //        break;
-    //    case 1:
-    //        switch (randCBook)
-    //        {
-    //        case 0:
-    //            db112.lb112.push_back(new Book);
-    //            break;
-    //        case 1:
-    //            db112.lb112.push_back(new Brochures);
-    //            break;
-    //        case 2:
-    //            db112.lb112.push_back(new Newspapers);
-    //            break;
-    //        case 3:
-    //            db112.lb112.push_back(new Magazines);
-    //            break;
-    //        default:
-    //            break;
-    //        }
-    //        break;
-    //    case 2:
-    //        switch (randCBook)
-    //        {
-    //        case 0:
-    //            db112.lb112.push_back(new Book);
-    //            db362.lb362.push_back(new Book);
-    //            break;
-    //        case 1:
-    //            db112.lb112.push_back(new Brochures);
-    //            db362.lb362.push_back(new Brochures);
-    //            break;
-    //        case 2:
-    //            db112.lb112.push_back(new Newspapers);
-    //            db362.lb362.push_back(new Newspapers);
-    //            break;
-    //        case 3:
-    //            db112.lb112.push_back(new Magazines);
-    //            db362.lb362.push_back(new Magazines);
-    //            break;
-    //        default:
-    //            break;
-    //        }
-    //        break;
-    //    default:
-    //        break;
-    //    }
-    //}
-    
+   
 }
 void PrintDB(string InputFileBD, string OutLib112, string OutLib362) {
     setlocale(LC_ALL, "ru");
@@ -266,13 +190,12 @@ void PrintDB(string InputFileBD, string OutLib112, string OutLib362) {
 int main()
 {
     string InputDataBase = "BD.txt";
-    string Library112 = "Lib112.txt";
+    Lib112 lb112;
+    Lib362 lb362;
     
-    fillDBLibs(InputDataBase, Library112, Library362);
-    PrintDB(InputDataBase, Library112, Library362);
+    fillDBLibs(InputDataBase, lb112.GetLib112() , lb362.GetLib362());
+    PrintDB(InputDataBase, lb112.GetLib112(), lb362.GetLib362());
   
-    //fillDBLib362(Bk.nameBook, Bk.nameBook , Bk.numberOfPages);
-    //map <T, vector <int>> lib362;
     
     return 0;
 }
